ae6f08521d82158833e8065e2dea23c70f539971
SyreIsLost <PauloRanger132@outlook.com>
2024-08-06 21:24:33 -0300

    Alterações no programaWeb

---
+++ JP WEB/Controllers/AlunoController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JP_WEB.Controllers
+{
+    public class AlunoController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
